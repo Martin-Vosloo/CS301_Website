d5e50448a51bbe5abe8394c0924ff577ef87972e
Nwabisa <ayandamalawu535@gmail.com>
2026-03-01 12:10:12 +0200

    updated placeholder on the booking page

--- JavaScript/slideshow.js
+++ JavaScript/slideshow.js
@@ -15,19 +15,19 @@ let image_slides1 = document.getElementsByClassName("home");
 // paths of images to slideshow, divvied up with their widths
 const images1 = ["../images/venue/reception_table0.jpg", "../images/venue/reception_table01.jpg","../images/venue/reception_table02.jpg","../images/venue/outside_seating0.jpg"];
 
-
-function slideshow1(){
+if(image_slides1.length > 0){
+    function slideshow1(){
      
-    // loop through each img and updates value stored in array( imgIndxe1)
-    for(i = 0; i < image_slides1.length;i++){
-         // if it reaches the end of the images goes back to start
-        imgIndex1[i] = imgIndex1[i]+1;
-        if(imgIndex1[i] >= images1.length){
-            imgIndex1[i] = 0;
-        }
-        //get the next img path from images2 list and assign it to current img element on the page
-        image_slides1[i].src = images1[imgIndex1[i]];
-        // image_slides1[i].src = images1[current1];
+        // loop through each img and updates value stored in array( imgIndxe1)
+        for(i = 0; i < image_slides1.length;i++){
+        // if it reaches the end of the images goes back to start
+            imgIndex1[i] = imgIndex1[i]+1;
+            if(imgIndex1[i] >= images1.length){
+                imgIndex1[i] = 0;
+            }
+            //get the next img path from images2 list and assign it to current img element on the page
+            image_slides1[i].src = images1[imgIndex1[i]];
+            // image_slides1[i].src = images1[current1];
     }
    
 }
@@ -35,6 +35,8 @@ function slideshow1(){
 
 // switch images after 3 seconds
     setInterval(slideshow1, 2000);
+
+}
 
 
 
@@ -52,16 +54,17 @@ let image_slides2 = document.getElementsByClassName("home1");
 // paths of images to slideshow, divvied up with their widths
 const images2 = ["../images/venue/outside_seating1.jpg","../images/venue/outside_night.jpg", "../images/venue/reception_hall2.jpg"];
 
-function slideshow2(){
+if(image_slides2.length > 0){
+    function slideshow2(){
     // loop through each img and updates value stored in array( imgIndxe2)
-    for(i = 0; i < image_slides2.length;i++){
-        imgIndex2[i] = imgIndex2[i]+1;
-        // if it reaches the end of the images goes back to start
-        if(imgIndex2[i] >= images2.length){
-            imgIndex2[i] = 0;
-        }
+        for(i = 0; i < image_slides2.length;i++){
+            imgIndex2[i] = imgIndex2[i]+1;
+            // if it reaches the end of the images goes back to start
+            if(imgIndex2[i] >= images2.length){
+                imgIndex2[i] = 0;
+            }
         //get the next img path from images2 list and assign it to current img element on the page
-        image_slides2[i].src = images2[imgIndex2[i]];
+            image_slides2[i].src = images2[imgIndex2[i]];
         
     }
 }
@@ -70,6 +73,8 @@ function slideshow2(){
 // switch images after 3 seconds
     setInterval(slideshow2, 2000);
 
+
+}
 
 
 /*-----------------------------
@@ -82,10 +87,14 @@ const locationUrl = "https://www.google.com/maps/place/Die+Boer+en+die+Belg,+Die
 
 // get button with a class name about-btn
 let locationBtn = document.getElementsByClassName("about-btn")[0];
-// when you click a button should take you to the location
-locationBtn.addEventListener("click", function(){
+
+if(locationBtn){
+    // when you click a button should take you to the location
+    locationBtn.addEventListener("click", function(){
     window.location.href = locationUrl;
 });
+}
+
 
 /*-----------------------------
 Change a button color when hover 
@@ -93,9 +102,25 @@ Change a button color when hover
 */
 let bgColor = "#a8c0a2";
 let bgColor2  = "#efefef";
-locationBtn.addEventListener("mouseenter", function(){
-    locationBtn.style.backgroundColor = bgColor;
-});
-locationBtn.addEventListener("mouseleave", function(){
-    locationBtn.style.backgroundColor = bgColor2;
-});
+
+if(locationBtn){
+    locationBtn.addEventListener("mouseenter", function(){
+        locationBtn.style.backgroundColor = bgColor;
+    });
+    locationBtn.addEventListener("mouseleave", function(){
+        locationBtn.style.backgroundColor = bgColor2;
+    });
+}
+
+
+/*-----------------------------
+placeholder property
+-----------------------------
+*/
+let placeholderVar = document.getElementById("couple-names");
+if(placeholderVar){
+    let names = "e.g. Samantha & Goerge";                             
+    placeholderVar.placeholder = names;
+}
+
+
