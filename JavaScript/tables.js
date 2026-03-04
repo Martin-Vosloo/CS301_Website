1a10081e7017231744547f1e5bf1fc1107e5495d
CModze <80900732+CModze@users.noreply.github.com>
2026-03-04 07:41:45 +0200

    JS to restyle the tables when clicked and to show the erpson's data if they are clicked

--- JavaScript/tables.js
+++ JavaScript/tables.js
@@ -1,20 +1,4 @@
 /*
-when you click on a table the table will change to an overflow and it will display everything based of the clicked styling
-it will also allow scroll in both directions
-*/
-
-function overlay(elementId) {
-    const elem = document.getElementsById(elementId);
-    if(elem.classList.contains("clicked")){
-        /*does have the class thus we must remove it*/
-        elem.classList.remove("clicked");
-    }else{
-        /* does not have the class thus we add it to it*/
-        elem.className += "clicked";
-    }
-}
-
-/*
     checking if the device is a mobile or not
     if it is we hide the edit buttons and display on their space that editing on mobile device can be errenious 
 */
@@ -29,11 +13,65 @@ function isMobile(){
     return screen&&touch&&keywords;
 }
 
+
+/*
+    reducing functionality basing that on the type of device being used
+*/
 window.addEventListener('load', function(){
     if(isMobile()){
         this.document.getElementById('changes').innerHTML=
         "Due to the nature of the device you are using to access the page we cannot allow you to make changes to the database.<br>Try using a desktop or laptop please";
         this.document.getElementById('changes').style.display="block";
-        this.document.getElementById('changes').style.width="100%";
+        this.document.getElementById('changes').style.width="90%";
     }
+})
+
+
+
+/*
+when you click on a table the table will change to an overflow and it will display everything based of the clicked styling
+it will also allow scroll in both directions
+*/
+
+function overlay(elementId) {
+    const elem = document.getElementById(elementId);
+    
+    if(elem.classList.contains("clicked")){
+       //removes th styling added using the style in js
+        elem.removeAttribute("style");
+        //removing the clicked stylnig class
+        elem.classList.remove("clicked");
+    }else{
+        /* does not have the class thus we add it to it*/
+        elem.classList.add("clicked");
+        /* 
+        the clicked class does not scale down nicely for small phone screens thus we make additional styling here
+        */
+       if(isMobile==true){
+            alert("is mobile ");
+            elem.style.top = "10%";
+            elem.style.marginBottom="26%";
+       }
+       
+    }
+}
+
+/*
+functionality to be added it the use of the transactions table
+*/
+const table = document.getElementById("currbooking");
+table.addEventListener("click", function(event){
+    //getting the closest row to the click on the table from the dom
+    const row = event.target.closest("tr");
+    //if none was found
+    if(!row) return;
+    //getting everythind or datavalue from the row clicked
+    const cells = row.querySelectorAll("td");
+    //changing from a node list to the usual array
+    const rowData = Array.from(cells).map(td=>td.textContent);
+    /*this is to be passed to php to display the person's transactions in the table that will appear on the side
+      this will be implemented later on
+    */
+
+
 })
