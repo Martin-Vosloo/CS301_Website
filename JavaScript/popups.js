89e78f045185e9e2d1efdf8da021d3a1029b7456
dcmcdonogh <dcmcdonogh@gmail.com>
2026-03-05 16:40:18 +0200

    fixed location & cookies banners

--- JavaScript/popups.js
+++ JavaScript/popups.js
@@ -1,26 +1,33 @@
 
 // get the cookies container, and two buttons
-  const container = document.getElementById("cookies-container");
-  const acceptBtn = document.getElementById("accept-cookies");
-  const rejectBtn = document.getElementById("reject-cookies");
+const languageContainer = document.getElementById("language-container");
+const acceptLanguageBtn = document.getElementById("accept-language-btn");
+acceptLanguageBtn.addEventListener("click", function() {
+  languageContainer.style.display = "none";
+});
+
+const cookiesContainer = document.getElementById("cookies-container");
+const rejectCookiesBtn = document.getElementById("reject-cookies-btn");
+const acceptCookiesBtn = document.getElementById("accept-cookies-btn");
 
   // Check if cookie exists, don't display popup if pre-accepted/exists
   if (document.cookie.includes("cookiesAccepted=true")) {
-    container.style.display = "none";
+    cookiesContainer.style.display = "none";
     console.log("Cookies Accepted")
   }
 
   // click to accept cookies, save changes and set expiry in 7 days; close container
-  acceptBtn.addEventListener("click", function() {
+  acceptCookiesBtn.addEventListener("click", function() {
     document.cookie = "cookiesAccepted=true; max-age=" + (60 * 60 * 24 * 7) + "; path=/";
-    container.style.display = "none";
+    cookiesContainer.style.display = "none";
   });
   
   // click to reject cookies, save changes and expire on refresh; close container
-  rejectBtn.addEventListener("click", function(){
+  rejectCookiesBtn.addEventListener("click", function(){
     document.cookie = "cookiesAccepted=false; path=/";
-    container.style.display = "none";
+    cookiesContainer.style.display = "none";
   });
+  
 
 function checkLanguage(){
   const userLanguage = navigator.language.substring(0, 2).toLowerCase();
@@ -40,6 +47,7 @@ function checkOnline(){
 
 if (checkLanguage()){
   console.log("User browser: English")
+  stateLanguage();
 }
 else {
   console.log("User browser: Not English")
