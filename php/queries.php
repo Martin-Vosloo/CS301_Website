921eab13db6543b72ca51c7492bbd8d3d81019a4
CModze <80900732+CModze@users.noreply.github.com>
2026-03-08 12:24:58 +0200

    php folder inside 3 files:connection, databseCreation, querExec

---
+++ php/queries.php
@@ -0,0 +1,7 @@
+<?php
+//this will be a function that will be used to execute all the queries possible,
+//this is intentionally done so to make it easier to restrict functions executed on the database
+
+function queryExecute($query){
+
+}
