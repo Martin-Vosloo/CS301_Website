6209d956bccef8d04e60e10723f8d81e5bf21d8c
CModze <80900732+CModze@users.noreply.github.com>
2026-03-09 14:00:59 +0200

    chnged the database strucure the the connection files

--- php/connection.php
+++ php/connection.php
@@ -1,29 +1,10 @@
 <?php
-class connecting{
-
-    private $server = "localhost";
-    private $user = "username";
-    private $password = "******";
-    private $conn;
-
-
-    //constructor
-    public __constructor(){
-        $this->connect();
-    }
-    //creates the db connection
-    private function connect(){
-        $this->conn = new mysqli($this->server, $this->user, $this->password);
-    }
-    
-    /*
-        logging  function will be used to log into the website
-        @parameters username and password
-    */
-    function logging($uname, $password){
-        $query = $this->conn->prepare("SELECT fname lname FROM users where pwrd = (SELECT ID FROM USERS WHERE ID = $uname OR $var=email)")
-    }
-    
-
-
+private $server = "localhost";
+private $user = "username";
+private $password = "******";
+private $conn;
+$this->conn = new mysqli($this->server, $this->user, $this->password);
+if($conn->connect_erorr){
+    die("Connection failed ". $conn->connect_erorr);
 }
+echo "Connected"
--- php/databaseStructure.sql
+++ php/databaseStructure.sql
@@ -1,42 +1,53 @@
+
+
 #creating the users table
 create table users(
 	identityNumber varchar(20) Primary Key,
-	CONSTRAINT checkId_len CHECK(CHAR_LENGTH(identityNumber) in (9,11,12,13,15,18) ),
+	constraint checkId_len CHECK(CHAR_LENGTH(identityNumber) in (9,11,12,13,15,18) ),
 	fname varchar(50),
 	#checkiong the first name using like to imitate regular expressions
 	constraint fname_format check(fname not like('%[^A-Za-z]%')),
 	lname varchar(50),
+    email_address varchar(40),
+	passwrd varchar(20),
     #checkiong the last name using like to imitate regular expressions
 	constraint lname_format check(lname not like('%[^A-Za-z]%')),
+    #checkinbg the format of the password
+    constraint word_length CHECK(char_length(pwrd)>7 and char_length(pwrd)<20),
+    #checking if the email is in the correct format
+    constraint email check(email not like'%[^A-Za-z0-9@._-]%' and email like '%@%' and email like '%.%'),
     #we are keeping the data not deleting user
 	alive bit default true
 );
 
-#creating the password table 
-create table pwrd(
-	id varchar(20) Primary Key,
-	CONSTRAINT checkId_len CHECK(CHAR_LENGTH(id) in (9,11,12,13,15,18) ),
-	pwrd varchar(20),
-	/*ensuring the length of the identity number is matched accordingly part of verifying that the person exists based of the length in the countries
-	with the following the length expected by the country:
-		United States, Canada, Australia, UK -9
-		Germany, Brazil, Nigeria, Russia-11, 
-		India, Japan-12, 
-		South Africa, South Korea-13, 
-		France-15
-		China, Mexico-18
-	*/
-	CONSTRAINT checkId_len CHECK(CHAR_LENGTH(id) in (9,11,12,13,15,18) ),
-	# the password length is set to be longer than 7 characters but less than 20 characters
-	CONSTRAINT word_length CHECK(char_length(pwrd)>7 and char_length(pwrd)<20),
-	# making sure the tables are linked and cascade accordingly
-	CONSTRAINT fk_identity FOREIGN KEY (id)
-	REFERENCES users(identityNumber)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
-);
+/*
+	moved to the user table
+	#creating the password table 
+	create table pwrd(
+		id varchar(20) Primary Key,
+		CONSTRAINT checkId_len CHECK(CHAR_LENGTH(id) in (9,11,12,13,15,18) ),
+		passwrd varchar(20),
+		/*ensuring the length of the identity number is matched accordingly part of verifying that the person exists based of the length in the countries
+		with the following the length expected by the country:
+			United States, Canada, Australia, UK -9
+			Germany, Brazil, Nigeria, Russia-11, 
+			India, Japan-12, 
+			South Africa, South Korea-13, 
+			France-15
+			China, Mexico-18
+		*/
+		CONSTRAINT checkId_len CHECK(CHAR_LENGTH(id) in (9,11,12,13,15,18) ),
+		# the password length is set to be longer than 7 characters but less than 20 characters
+		CONSTRAINT word_length CHECK(char_length(pwrd)>7 and char_length(pwrd)<20),
+		# making sure the tables are linked and cascade accordingly
+		CONSTRAINT fk_identity FOREIGN KEY (id)
+		REFERENCES users(identityNumber)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+	);
+*/
 
-#creatin the admin table
+#creating the admin table
 create table admin(
 	idNo varchar(20) PRIMARY KEY,
 	CONSTRAINT checkId_len CHECK(CHAR_LENGTH(idNo) in (9,11,12,13,15,18) ),
@@ -74,6 +85,15 @@ create table venue(
 	venueName varchar(20),
     location varchar(50),
     capacity integer
+);
+
+#creating the reviews table
+create table reviews(
+	id int primary key,
+    person varchar(100),
+    text_review varchar(300),
+    image varchar(100),
+    constraint imageLink check(image like '%.pnj' or image like'%jpg')
 );
 
 #Creating the blocked
