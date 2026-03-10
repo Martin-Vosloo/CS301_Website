3334cdeb9a1c2d3736a666f536006ece0c682e86
CModze <80900732+CModze@users.noreply.github.com>
2026-03-10 12:41:13 +0200

    connection changed

--- php/connection.php
+++ php/connection.php
@@ -2,14 +2,12 @@
 $server = 'CS3-DEV.ICT.RU.AC.ZA';
 $user = "G21M4333";
 $password = "ModChu21";
-$dbname = 'group3';
 $conn;
 $conn = new mysqli($server, $user, $password, $dbname);
 if($conn->connect_error){
-    die("Connection failed ". $conn->connect_erorr);
+    header("HTTP/1.1 404 Not Found");
+    die();
 }
-echo "Connected";
-
 
 function clean($text){
     return htmlspecialchars(stripslashes(trim($data)));
