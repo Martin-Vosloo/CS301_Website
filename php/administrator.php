7a893cb89955531b7c9e62e7b4a9896e184dc577
CModze <80900732+CModze@users.noreply.github.com>
2026-03-11 08:47:55 +0200

    admin

--- php/administrator.php
+++ php/administrator.php
@@ -1,6 +1,7 @@
 <?php
     require_once "connection.php";
-    //this will return an arrao of 2 dimensions 
+
+    //this will return an arrao of 2 dimensions with the records in them and entered as the rows accordingly
     function table($feild = "fname", $order = "ASC", $sign = "<" ){
         $sql = "SELECT fname, lname, start_date, duration 
                 FROM users inner join booking on users.identityNumber=booking.idNo 
@@ -20,3 +21,28 @@
         }
         return $rows;
     }
+    ?>
+    <script>
+        //getting the table results 
+        const records = <?php echo table() ?>;
+        //
+
+
+        //this creates the table 
+        function buildTable(records) {
+            // let html = "<table border='0'>";
+            records.forEach(row => {
+                html += "<tr>";
+                row.forEach(cell => {
+                    html += "<td>" + cell + "</td>";
+                });
+                html += "</tr>";
+            });
+
+            html += "</table>";
+            return html;
+        }
+        //adding dtaa top the table
+        function initialTable_onload(){
+            document.getElementById("curbooking").innerhtml = buildTable(records);
+        }
