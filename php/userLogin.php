e85138005291df74b32de5d64e7a2d3b6081f38f
dcmcdonogh <dcmcdonogh@gmail.com>
2026-03-11 11:28:19 +0200

    php userSignup moved to signU.php

--- php/userLogin.php
+++ php/userLogin.php
@@ -1,53 +1,52 @@
 <?php
     $error = "";
-    private function create_userid(){
-        $length = rand(4, 20);
-        $number = "";
-        for($i=0; $i < $length; $i++){
-            $new_rand = rand(0, 9);
-            $number = $number . $new_rand;
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        if (!$DB = new PDO("mysql:host=localhost;dbname=XXXX", "root", ""))
+        {
+            die ("Could not connect to database");
         }
-        return $number;
-    }
-    
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    if (!$DB = new PDO("mysql:host=localhost;dbname=XXXX", "root", "")) {
-        die ("Could not connect to database");
-    }
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
 
-    $arr['user_id'] = create_userid();
-    
-    $query = "select * from users where user_id = :user_id limit 1";
-    $stm = $DB->prepare($query)
+        $arr['email'] = $_POST['email'];
+        $arr['password'] = hash('sha1', $_POST['password']);
+        
+        $query = "select * from users where user_id = :user_id limit 1";
+        $stm = $DB->prepare($query)
 
-    if ($stm) {
-        $check = $stm->execute($arr);
-        if ($check) {
-            $data = $stm->fetchAll(PDO::FETCH_ASSOC);
-            if (is_array($data) && count($data) > 0) {
+        if ($stm)
+        {
+            $check = $stm->execute($arr);
+
+            if ($check)
+            {
+                $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+                if (is_array($data) && count($data) > 0)
+                {
+                    $_SESSION['myid'] = $data[0]['user_id'];
+                }
+                else
+                {
+                    $error = "Wrong username or password";
+                }
                 
             }
-            # code...
         }
-    }
 
-    $arr['email_address'] = $_POST['email_address'];
-    $arr['passwrd'] = hash('sha1', $POST['passwrd']);
-    
-    $query = "select * from users where email_address = :email_address && passwrd = :passwrd limit 1";
-    $stm = $DB->prepare($query)
-    
-    if ($stm) {
-        $check = $stm->execute($arr);
-        if (!$check) {
-            $error = "Wrong username or password";
-        }
-        if ($error = ""){
-            header("Location: index.php")
+        $arr['email_address'] = $_POST['email_address'];
+        $arr['passwrd'] = hash('sha1', $POST['passwrd']);
+        
+        $query = "select * from users where email_address = :email_address && passwrd = :passwrd limit 1";
+        $stm = $DB->prepare($query)
+        
+        if ($stm) {
+            $check = $stm->execute($arr);
+            if (!$check) {
+                
+            }
+            if ($error = ""){
+                header("Location: index.php")
+            }
         }
     }
-}
 ?>
