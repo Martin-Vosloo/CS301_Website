26ef3a39e835dd43ddb88b0b590ac1961c69528e
CModze <80900732+CModze@users.noreply.github.com>
2026-03-12 00:28:29 +0200

    reporting debugging

--- php/connection.php
+++ php/connection.php
@@ -3,8 +3,7 @@ $server = 'CS3-DEV.ICT.RU.AC.ZA';
 $user = 'G21M4333';
 $password = 'ModChu21';
 $dbname = 'group3';
-$conn;
-$conn = new mysqli($server, $user);
+$conn = new mysqli($server, $user, $password, $dbname);
 if ($conn->connect_error) {
     header('HTTP/1.1 404 Not Found');
     die();
