a1773ec798f095d0b944dc16aa6e7722d36f447f
Martin Vosloo <G24V5071@ict.ru.ac.za>
2026-03-11 21:17:34 +0200

    admin style

--- html/administrator.php
+++ html/administrator.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/admin.css" />
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="../css/style_ab.css" />
+    <link rel="stylesheet" href="../css/style.css" />
     <script src="/JavaScript/tables.js"></script>
   </head>
 
