695984dbfdbc94a03883d98f78bdfe5b5ee20917
dcmcdonogh <dcmcdonogh@gmail.com>
2026-03-12 00:26:22 +0200

    signup update add logout

---
+++ html/profile.php
@@ -0,0 +1,4 @@
+<!-- CREATE A USER PROFILE HERE IN THE FUTURE -->
+ <?php
+echo 'This is soon to be a profile page for profile settings... in the future'
+?>
--- php/connection.php
+++ php/connection.php
@@ -4,16 +4,17 @@ $user = 'G21M4333';
 $password = 'ModChu21';
 $dbname = 'group3';
 $conn;
-$conn = new mysqli($server, $user, $password);
+$conn = new mysqli($server, $user, $password, $dbname);
 if ($conn->connect_error) {
     header('HTTP/1.1 404 Not Found');
     die();
 }
 // echo 'Connected';
 
-function clean($text){
-    //using cleaner class and the usual stuff for the forms
+function clean($text)
+{
+    // using cleaner class and the usual stuff for the forms
     // $conf = HTMLPurifier_Config::createDefault();
     // $purifier = new HTMLPurifier($conf);
-    return htmlspecialchars(stripslashes(trim(/*$purifier->purify(*/$text)));
+    return htmlspecialchars(stripslashes(trim(/* $purifier->purify( */ $text)));
 }
