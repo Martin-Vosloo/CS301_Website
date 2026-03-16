b2dc1dd04ef89fc4b7bfef76608b4d729009e1c1
Nwabisa <ayandamalawu535@gmail.com>
2026-03-16 17:31:57 +0200

    added booking confirmation email  feature

--- php/connection.php
+++ php/connection.php
@@ -1,17 +1,26 @@
 <?php
-$server = 'CS3-DEV.ICT.RU.AC.ZA';
-$user = 'G21M4333';
-$password = 'ModChu21';
-$dbname = 'group3';
-$conn = new mysqli($server, $user, $password, $dbname);
-if ($conn->connect_error) {
-    header('HTTP/1.1 404 Not Found');
-    die();
-}
+// Point tot the vendor folder in your root
+require_once  dirname(__DIR__) . '/vendor/autoload.php';
+
+// load the .env file from the root
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+// $server = 'CS3-DEV.ICT.RU.AC.ZA';
+// $user = 'G21M4333';
+// $password = 'ModChu21';
+// $dbname = 'group3';
+// $conn = new mysqli($server, $user, $password, $dbname);
+// if ($conn->connect_error) {
+//     header('HTTP/1.1 404 Not Found');
+//     die();
+// }
 // echo 'Connected';
 
 function clean($text)
 {
     return htmlspecialchars(stripslashes(trim($text)), ENT_QUOTES, 'UTF-8');
 }
+
+
 ?>
